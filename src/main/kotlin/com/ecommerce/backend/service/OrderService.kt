fc1bb4c31f60b4bfd401a5f428cddb65bb268eea
Pranav Padwale <your-email@example.com>
2026-01-08 16:51:27 +0530

    Implemented user cart functionality including add-to-cart and view-cart APIs with JWT-based authentication. Developed order placement workflow from cart and enabled order history retrieval for authenticated users. Integrated and validated cart-to-order flow by handling product lookup and resolving product consistency issues. Performed debugging, validation, and Postman testing to ensure end-to-end cart and order APIs function correctly.

--- src/main/kotlin/com/ecommerce/backend/service/OrderService.kt
+++ src/main/kotlin/com/ecommerce/backend/service/OrderService.kt
@@ -3,12 +3,15 @@ package com.ecommerce.backend.service
 import com.ecommerce.backend.model.Order
 import com.ecommerce.backend.repository.CartRepository
 import com.ecommerce.backend.repository.OrderRepository
+import com.ecommerce.backend.repository.ProductRepository
 import org.springframework.stereotype.Service
 
 @Service
 class OrderService(
     private val cartRepository: CartRepository,
-    private val orderRepository: OrderRepository
+    private val orderRepository: OrderRepository,
+    private val productRepository: ProductRepository
+
 ) {
 
     fun placeOrder(userEmail: String): Order {
@@ -20,14 +23,34 @@ class OrderService(
             throw RuntimeException("Cart is empty")
         }
 
+        cart.items.forEach { item ->
+            val product = productRepository.findById(item.productId)
+                .orElseThrow { throw RuntimeException("Product not found") }
+
+            if (product.stock < item.quantity) {
+                throw RuntimeException("Insufficient stock for product ${product.name}")
+
+            }
+
+            val updateProduct = product.copy(
+                stock = product.stock - item.quantity,
+            )
+
+            productRepository.save(updateProduct)
+
+
+        }
+
         val order = Order(
             userEmail = userEmail,
             items = cart.items
         )
 
+        val savedOrder = orderRepository.save(order)
+
         cartRepository.save(cart.copy(items = emptyList()))
 
-        return orderRepository.save(order)
+        return savedOrder
     }
 
     fun getOrders(userEmail: String): List<Order> {
